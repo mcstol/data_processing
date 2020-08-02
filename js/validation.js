2e8f4f8983ef6b01149639a14d7ec2447c3ef39e
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2020-08-02 15:32:21 -0300

    login interface

---
+++ js/validation.js
@@ -0,0 +1,6 @@
+function emailValidation(event){
+    digitEmail = document.getElementById("aligned-email").value
+    if (digitEmail == "teste@email.com"){
+        console.log("e-mail valido")
+    }
+}
