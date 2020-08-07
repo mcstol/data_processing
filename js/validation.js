286e4542ed2496fcef3f80e2f437dcbec4f88ad4
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2020-08-07 17:49:49 -0300

    email validation teste

--- js/validation.js
+++ js/validation.js
@@ -1,6 +1,22 @@
 function emailValidation(event){
-    digitEmail = document.getElementById("aligned-email").value
+    digitEmail = $("#aligned-email").val()
     if (digitEmail == "teste@email.com"){
         console.log("e-mail valido")
+    }
+    else{
+        alert("email incorreto")
+    }
+}
+function passwordValidation(event){
+    //event.preventDefault()
+    digitPassword = document.getElementById("aligned-password").value
+    if (digitPassword == "12345"){
+        console.log("chamou outra pagina")
+       //window.open(/DashBoard.html)
+        location.replace("/DashBoard.html");
+
+    }
+    else{
+        alert("Senha incorreta!")
     }
 }
